4254f73d49f92229f46834b21e3242ad1b8a760d
volodymyr_krokhmaliuk <krokhmalyuk777@gmail.com>
2022-09-02 19:48:54 +0300

    fix pr comments

--- src/com/lviv/lits/Main.java
+++ src/com/lviv/lits/Main.java
@@ -3,6 +3,6 @@ package com.lviv.lits;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello from task1");
+        System.out.println("Hello from task1.1");
     }
 }
