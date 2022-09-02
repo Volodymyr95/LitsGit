b88cfa652cef7b4a60e6f79a749cfddb7ea849ce
volodymyr_krokhmaliuk <krokhmalyuk777@gmail.com>
2022-09-02 20:00:55 +0300

    4 commit

--- src/com/lviv/lits/Main.java
+++ src/com/lviv/lits/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello with rebase");
         System.out.println("Hello world!");
         System.out.println(2+2*2);
+        System.out.println("End!");
     }
 }
